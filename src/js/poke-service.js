b55c16bc883a019739205c08b33c1c4b50493edb
Thomas Russell <t.p.russell19@gmail.com>
2021-04-21 11:13:24 -0700

    add initial files plus API call
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

---
+++ src/js/poke-service.js
@@ -0,0 +1,14 @@
+export default class PokeService {
+  static getPokemon(pokemon) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
+}
