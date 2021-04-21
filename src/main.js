e530cc134c1fe27b40cf203276de09aa270e16d4
Thomas Russell <t.p.russell19@gmail.com>
2021-04-21 11:53:07 -0700

    WIP: trying to display results from succesful API call
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@ import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
+import PokeService from './js/poke-service.js'
 
-$("section").text("TEST TEXT INPUT")
+function showPokemon(response) {
+  if (response) {
+    console.log(response)
+    $('#poke-results').text(`${response.name}`)
+  }
+}
+
+$(document).ready(function() {
+  $('#poke').submit(function(event) {
+    event.preventDefault();
+    let pokemon = $('#pokemon').val();
+    PokeService.getPokemon(pokemon)
+    console.log(pokemon)
+      .then(function(response) {
+        showPokemon(response);
+      });
+  });
+});
